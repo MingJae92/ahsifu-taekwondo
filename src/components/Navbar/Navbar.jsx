7e9ecc3f960965d1b4047e36f76644049412a646
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-11-08 00:12:44 +0000

    Animations added to nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Box, Button, IconButton, Menu, MenuItem, Divider } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  Button,
-  IconButton,
-  Menu,
-  MenuItem,
-  Divider,
-} from "@mui/material";
+import { motion } from "framer-motion";  // Import motion from Framer Motion
 
 function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -24,63 +15,57 @@ function Navbar() {
   };
 
   return (
-    <AppBar 
-      position="fixed"
-      color="transparent"
-      elevation={0}
-      sx={{ width: "100%", top: 0 }}
+    <motion.div
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
     >
-      <Toolbar sx={{ justifyContent: "space-between", px: { xs: 2, md: 5 } }}>
-        {/* Logo */}
-        <Typography variant="h6" color="primary" sx={{ fontWeight: "bold" }}>
-          DENTON
-        </Typography>
+      <AppBar position="fixed" color="transparent" elevation={0} sx={{ width: "100%", top: 0 }}>
+        <Toolbar sx={{ justifyContent: "space-between", px: { xs: 2, md: 5 } }}>
+          {/* Logo */}
+          <Typography variant="h6" color="primary" sx={{ fontWeight: "bold" }}>
+            DENTON
+          </Typography>
 
-        {/* Desktop Menu */}
-        <Box sx={{ display: { xs: "none", md: "flex" }, gap: 3 }}>
-          {["Home", "About", "Services", "Our Work", "Blog", "Contact"].map(
-            (item) => (
-              <Button
-                key={item}
-                color="inherit"
-                sx={{ fontSize: "1rem", textTransform: "none" }}
-              >
+          {/* Desktop Menu */}
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 3 }}>
+            {["Home", "About", "Services", "Our Work", "Blog", "Contact"].map((item) => (
+              <Button key={item} color="inherit" sx={{ fontSize: "1rem", textTransform: "none" }}>
                 {item}
               </Button>
-            )
-          )}
-        </Box>
-
-        {/* Sign Up and Log In Buttons */}
-        <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
-          <Button variant="outlined" color="primary" sx={{ textTransform: "none" }}>
-            Log In
-          </Button>
-          <Button variant="contained" color="primary" sx={{ textTransform: "none" }}>
-            Sign Up
-          </Button>
-        </Box>
-
-        {/* Mobile Menu Icon */}
-        <Box sx={{ display: { xs: "flex", md: "none" } }}>
-          <IconButton edge="start" color="inherit" onClick={handleMenuOpen}>
-            <MenuIcon />
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-            {["Home", "About", "Services", "Our Work", "Blog", "Contact"].map((item) => (
-              <MenuItem key={item} onClick={handleMenuClose}>
-                {item}
-              </MenuItem>
             ))}
-            <Divider />
-            {/* Mobile Log In and Sign Up Options */}
-            <MenuItem onClick={handleMenuClose}>Log In</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Sign Up</MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
-      <Divider />
-    </AppBar>
+          </Box>
+
+          {/* Sign Up and Log In Buttons */}
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
+            <Button variant="outlined" color="primary" sx={{ textTransform: "none" }}>
+              Log In
+            </Button>
+            <Button variant="contained" color="primary" sx={{ textTransform: "none" }}>
+              Sign Up
+            </Button>
+          </Box>
+
+          {/* Mobile Menu Icon */}
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton edge="start" color="inherit" onClick={handleMenuOpen}>
+              <MenuIcon />
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+              {["Home", "About", "Services", "Our Work", "Blog", "Contact"].map((item) => (
+                <MenuItem key={item} onClick={handleMenuClose}>
+                  {item}
+                </MenuItem>
+              ))}
+              <Divider />
+              <MenuItem onClick={handleMenuClose}>Log In</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Sign Up</MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+        <Divider />
+      </AppBar>
+    </motion.div>
   );
 }
 
